kurtbahartr/android_device_samsung_m34x

a25x: Add product build prop overrides
Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38
Signed-off-by: ksawlii <ksawery.blaszczak@proton.me>

lineage_a25x.mk
@@ -38,3 +38,9 @@ PRODUCT_MANUFACTURER := samsung
 PRODUCT_SHIPPING_API_LEVEL := 34
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="a25xdxx-user 15 AP3A.240905.015.A2 A256EXXU7CYE1 release-keys" \
+    BuildFingerprint=samsung/a25xdxx/essi:15/AP3A.240905.015.A2/A256EXXU7CYE1:user/release-keys \
+    DeviceProduct=a25xdxx \
+    SystemName=a25xdxx

akifakif32/android_device_samsung_a25x

Signed-off-by: Ksawlii <maja.blaszczak@proton.me>

lineage_a25x.mk
@@ -33,3 +33,9 @@ PRODUCT_MODEL := SM-A256E
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_SHIPPING_API_LEVEL := 34
 PRODUCT_CHARACTERISTICS := phone
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="a25xdxx-user 15 AP3A.240905.015.A2 A256EXXS7CYG1 release-keys" \
+    BuildFingerprint=samsung/a25xdxx/essi:15/AP3A.240905.015.A2/A256EXXS7CYG1:user/release-keys \
+    DeviceProduct=a25xdxx \
+    SystemName=a25xdxx