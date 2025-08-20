b8e4d97136456b1f47a5b554f3bd80b17db5348b
Ksawlii <maja.blaszczak@proton.me>
2025-08-20 23:28:48 +0200

    a25x: props: Move model-specific props to sku
    Change-Id: I0e344cb71b15a9ac8f517200bcb1f7521f7701d9
    Signed-off-by: Ksawlii <maja.blaszczak@proton.me>

--- device.mk
+++ device.mk
@@ -65,18 +65,18 @@ PRODUCT_COPY_FILES += \
 
 # Variant props
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/props/odm_a25x/odm_SM-A2560.prop:$(TARGET_COPY_OUT_ODM)/build_SM-A2560.prop \
-    $(DEVICE_PATH)/configs/props/odm_a25x/odm_SM-A256B.prop:$(TARGET_COPY_OUT_ODM)/build_SM-A256B.prop \
-    $(DEVICE_PATH)/configs/props/odm_a25x/odm_SM-A256E.prop:$(TARGET_COPY_OUT_ODM)/build_SM-A256E.prop \
-    $(DEVICE_PATH)/configs/props/product_a25x/product_SM-A2560.prop:$(TARGET_COPY_OUT_PRODUCT)/build_SM-A2560.prop \
-    $(DEVICE_PATH)/configs/props/product_a25x/product_SM-A256B.prop:$(TARGET_COPY_OUT_PRODUCT)/build_SM-A256B.prop \
-    $(DEVICE_PATH)/configs/props/product_a25x/product_SM-A256E.prop:$(TARGET_COPY_OUT_PRODUCT)/build_SM-A256E.prop \
-    $(DEVICE_PATH)/configs/props/system_a25x/system_SM-A2560.prop:$(TARGET_COPY_OUT_SYSTEM)/build_SM-A2560.prop \
-    $(DEVICE_PATH)/configs/props/system_a25x/system_SM-A256B.prop:$(TARGET_COPY_OUT_SYSTEM)/build_SM-A256B.prop \
-    $(DEVICE_PATH)/configs/props/system_a25x/system_SM-A256E.prop:$(TARGET_COPY_OUT_SYSTEM)/build_SM-A256E.prop \
-    $(DEVICE_PATH)/configs/props/system_ext_a25x/system_ext_SM-A2560.prop:$(TARGET_COPY_OUT_SYSTEM_EXT)/build_SM-A2560.prop \
-    $(DEVICE_PATH)/configs/props/system_ext_a25x/system_ext_SM-A256B.prop:$(TARGET_COPY_OUT_SYSTEM_EXT)/build_SM-A256B.prop \
-    $(DEVICE_PATH)/configs/props/system_ext_a25x/system_ext_SM-A256E.prop:$(TARGET_COPY_OUT_SYSTEM_EXT)/build_SM-A256E.prop \
-    $(DEVICE_PATH)/configs/props/vendor_a25x/vendor_SM-A2560.prop:$(TARGET_COPY_OUT_VENDOR)/build_SM-A2560.prop \
-    $(DEVICE_PATH)/configs/props/vendor_a25x/vendor_SM-A256B.prop:$(TARGET_COPY_OUT_VENDOR)/build_SM-A256B.prop \
-    $(DEVICE_PATH)/configs/props/vendor_a25x/vendor_SM-A256E.prop:$(TARGET_COPY_OUT_VENDOR)/build_SM-A256E.prop
+    $(DEVICE_PATH)/configs/props/sku/odm_a25x/odm_SM-A2560.prop:$(TARGET_COPY_OUT_ODM)/build_SM-A2560.prop \
+    $(DEVICE_PATH)/configs/props/sku/odm_a25x/odm_SM-A256B.prop:$(TARGET_COPY_OUT_ODM)/build_SM-A256B.prop \
+    $(DEVICE_PATH)/configs/props/sku/odm_a25x/odm_SM-A256E.prop:$(TARGET_COPY_OUT_ODM)/build_SM-A256E.prop \
+    $(DEVICE_PATH)/configs/props/sku/product_a25x/product_SM-A2560.prop:$(TARGET_COPY_OUT_PRODUCT)/build_SM-A2560.prop \
+    $(DEVICE_PATH)/configs/props/sku/product_a25x/product_SM-A256B.prop:$(TARGET_COPY_OUT_PRODUCT)/build_SM-A256B.prop \
+    $(DEVICE_PATH)/configs/props/sku/product_a25x/product_SM-A256E.prop:$(TARGET_COPY_OUT_PRODUCT)/build_SM-A256E.prop \
+    $(DEVICE_PATH)/configs/props/sku/system_a25x/system_SM-A2560.prop:$(TARGET_COPY_OUT_SYSTEM)/build_SM-A2560.prop \
+    $(DEVICE_PATH)/configs/props/sku/system_a25x/system_SM-A256B.prop:$(TARGET_COPY_OUT_SYSTEM)/build_SM-A256B.prop \
+    $(DEVICE_PATH)/configs/props/sku/system_a25x/system_SM-A256E.prop:$(TARGET_COPY_OUT_SYSTEM)/build_SM-A256E.prop \
+    $(DEVICE_PATH)/configs/props/sku/system_ext_a25x/system_ext_SM-A2560.prop:$(TARGET_COPY_OUT_SYSTEM_EXT)/build_SM-A2560.prop \
+    $(DEVICE_PATH)/configs/props/sku/system_ext_a25x/system_ext_SM-A256B.prop:$(TARGET_COPY_OUT_SYSTEM_EXT)/build_SM-A256B.prop \
+    $(DEVICE_PATH)/configs/props/sku/system_ext_a25x/system_ext_SM-A256E.prop:$(TARGET_COPY_OUT_SYSTEM_EXT)/build_SM-A256E.prop \
+    $(DEVICE_PATH)/configs/props/sku/vendor_a25x/vendor_SM-A2560.prop:$(TARGET_COPY_OUT_VENDOR)/build_SM-A2560.prop \
+    $(DEVICE_PATH)/configs/props/sku/vendor_a25x/vendor_SM-A256B.prop:$(TARGET_COPY_OUT_VENDOR)/build_SM-A256B.prop \
+    $(DEVICE_PATH)/configs/props/sku/vendor_a25x/vendor_SM-A256E.prop:$(TARGET_COPY_OUT_VENDOR)/build_SM-A256E.prop
