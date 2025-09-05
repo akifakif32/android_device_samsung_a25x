a25c2c12054214f5940b0d1287909ce99247e8c3
Majaahh <maja.blaszczak@proton.me>
2025-09-05 08:53:26 +0200

    a25x: nfc: Import nci config
    Change-Id: I8b6391ebb4c77c353e9ef8cfaf48f791f11328db
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- device.mk
+++ device.mk
@@ -46,7 +46,9 @@ PRODUCT_COPY_FILES += \
 # Nfc
 PRODUCT_PACKAGES += android.hardware.nfc@1.2-service.st
 
-PRODUCT_COPY_FILES += $(DEVICE_PATH)/configs/nfc/libnfc-hal-st.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-hal-st.conf
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
+    $(DEVICE_PATH)/configs/nfc/libnfc-hal-st.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-hal-st.conf
 
 # Manifest
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
