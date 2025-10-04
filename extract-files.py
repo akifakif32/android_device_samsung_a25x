2fb0c53cc78cb5ed4fed9579dba689816435cf1d
Majaahh <maja.blaszczak@proton.me>
2025-10-04 17:20:21 +0200

    a25x: Drop unnecessary hermesd fixup
    vendor/bin/hermesd: file expected to be fixed up, but pre-fixup hash and post-fixup hash are the same
    
    Change-Id: I37a8ddb33b9275dcfb0196056ef741561e0c6a3b
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -40,9 +40,6 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/libexynoscamera3.so': blob_fixup()
         .add_needed('libshim_camera.so')
         .binary_regex_replace(b'_ZN7android5Fence', b'_ZN7exynos55Fence'),
-    # Hermes
-    'vendor/bin/hermesd': blob_fixup()
-        .binary_regex_replace(rb'ro\.factory\.factory_binary', b'ro.vendor.factory_binary\x00'),
     # Keymint
     (
         'vendor/bin/hw/android.hardware.security.keymint-service.samsung',
